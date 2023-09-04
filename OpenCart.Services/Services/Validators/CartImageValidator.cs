89d013dcd2f5bd7c08928a83a46b3ee5cce67a58
martinkleinbooi@gmail.com <jackdaniels@123>
2023-09-04 19:30:08 +0200

    - Added validators for models

---
+++ OpenCart.Services/Services/Validators/CartImageValidator.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using OpenCart.Models.Entities;
+
+namespace OpenCart.Services.Services.Validators
+{
+    public class CartImageValidator : AbstractValidator<CartItemImage>
+    {
+        public CartImageValidator()
+        {
+            RuleFor(image => image.FileName)
+                .NotEmpty().WithMessage("File name is required.")
+                .MaximumLength(100).WithMessage("File name must not exceed 100 characters.");
+
+            RuleFor(image => image.FileBytes)
+                .NotEmpty().WithMessage("File bytes are required.");
+
+            RuleFor(image => image.ContentType)
+                .NotEmpty().WithMessage("Content type is required.")
+                .MaximumLength(50).WithMessage("Content type must not exceed 50 characters.")
+                .Must(HaveValidImageContentType).WithMessage("Invalid image content type. Supported types are JPEG, PNG, and GIF.");
+
+            RuleFor(image => image.Description)
+                .MaximumLength(100).WithMessage("Description must not exceed 100 characters.");
+
+            RuleFor(image => image.Length)
+                .GreaterThan(0).WithMessage("Length must be greater than 0.");
+
+            RuleFor(image => image.Name)
+                .MaximumLength(50).WithMessage("Name must not exceed 50 characters.");
+        }
+
+        private bool HaveValidImageContentType(string contentType)
+        {
+            if (string.IsNullOrWhiteSpace(contentType))
+                return false;
+
+            var validContentTypes = new List<string>
+            {
+                "image/jpeg",
+                "image/png",
+                "image/gif"
+            };
+
+            return validContentTypes.Contains(contentType);
+        }
+    }
+}
---
+++ OpenCart.Services/Services/Validators/UserValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart.Services.Services.Validators
+{
+    public class UserValidator : AbstractValidator<Application>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .EmailAddress().WithMessage("Username must be a valid email address.");
+
+            RuleFor(user => user.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(user => user.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(20).WithMessage("First name must not exceed 20 characters.");
+
+            RuleFor(user => user.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(20).WithMessage("Last name must not exceed 20 characters.");
+        }
+    }
+    
+    
+}
