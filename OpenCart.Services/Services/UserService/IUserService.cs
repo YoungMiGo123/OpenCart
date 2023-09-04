d5e4b68dfd89b156566c848baef7a1f612945bf0
martinkleinbooi@gmail.com <jackdaniels@123>
2023-09-04 19:30:39 +0200

    - Added user service

---
+++ OpenCart.Services/Services/UserService/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using OpenCart.Models.Entities;
+
+namespace OpenCart.Services.Services.UserService
+{
+    public interface IUserService
+    {
+        Task<ServiceResponse<ApplicationUser>> CreateAsync(Domain.User user);
+        Task<bool> ExistAsync(string identityName);
+    }
+}
---
+++ OpenCart.Services/Services/UserService/UserService.cs
@@ -0,0 +1,28 @@
+﻿using OpenCart.Common;
+using OpenCart.Models.Entities;
+using OpenCart.Repositories.Repositories.UserRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart.Services.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        public UserService(IUserRepository userRepository)
+        {
+
+        }
+        public Task<ServiceResult<ApplicationUser>> CreateUserAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExistAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
