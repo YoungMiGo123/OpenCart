e1ee2f502d3d10cf4a7e3f659447d85df382226a
martinkleinbooi@gmail.com <jackdaniels@123>
2023-09-04 21:20:39 +0200

    - Updated the cart item image repository

---
+++ OpenCart.Repositories/Repositories/CartItemImageRepository/CartItemImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using OpenCart.Infrastructure.Context;
+using OpenCart.Models.Entities;
+using OpenCart.Repositories.Repositories.CartItemImageRepository;
+
+namespace OpenCart.Repositories.Repositories.CartItemRepository
+{
+    public class CartItemImageRepository : GenericRepository<CartItemImage>, ICartItemImageRepository
+    {
+        public CartItemImageRepository(OpenCartDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ OpenCart.Repositories/Repositories/CartItemImageRepository/ICartItemImageRepository.cs
@@ -0,0 +1,9 @@
+﻿using OpenCart.Models.Entities;
+using OpenCart.Repositories.Repositories.GenericRepository;
+
+namespace OpenCart.Repositories.Repositories.CartItemImageRepository
+{
+    public interface ICartItemImageRepository : IGenericRepository<CartItemImage>
+    {
+    }
+}
