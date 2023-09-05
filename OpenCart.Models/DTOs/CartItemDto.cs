5d24523d03052500864e920027ed07960abae3e4
martinkleinbooi@gmail.com <jackdaniels@123>
2023-09-05 19:52:27 +0200

    - Added dtos

---
+++ OpenCart.Models/DTOs/CartItemDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart.Models.DTOs
+{
+    public class CartItemDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ OpenCart.Models/DTOs/CartItemImageDto.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenCart.Models.DTOs
+{
+    public class CartItemImageDto
+    {
+        public Guid Id { get; set; }
+        public string? FileName { get; set; }
+        public byte[]? FileBytes { get; set; }
+        public string? ContentType { get; set; }
+        public string? Description { get; set; }
+        public long Length { get; set; }
+        public string? Name { get; set; }
+    }
+}
