bb23a9a6a7f430bd50a7f9fb464d92f133a23283
martinkleinbooi@gmail.com <jackdaniels@123>
2023-09-05 21:51:43 +0200

    - Added mappings

---
+++ OpenCart.Queries/Mapping/CartItemImageMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OpenCart.Models.DTOs;
+using OpenCart.Models.Entities;
+
+namespace OpenCart.Operations.Mapping
+{
+    public class CartItemImageMapping : Profile
+    {
+        public CartItemImageMapping()
+        {
+            CreateMap<CartItemImage, CartItemImageDto>();
+            CreateMap<CartItemDto, CartItem>();
+        }
+    }
+}
---
+++ OpenCart.Queries/Mapping/CartItemMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using OpenCart.Models.DTOs;
+using OpenCart.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart.Operations.Mapping
+{
+    public class CartItemMapping : Profile
+    {
+        public CartItemMapping()
+        {
+            CreateMap<CartItem, CartItemDto>();
+            CreateMap<CartItemDto, CartItem>();
+        }
+    }
+ 
+}
